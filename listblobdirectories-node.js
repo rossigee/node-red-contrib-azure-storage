c66e413bf0a1568d81e5b09f27f58815d5b02b27
Ross Golder <ross@golder.org>
2017-07-30 13:50:31 +0700

    Add 'ListBlobDirectories' functionality.

---
+++ listblobdirectories-node.js
@@ -0,0 +1,68 @@
+module.exports = function (RED) {
+    function AzureStorageListBlobDirectoriesNode(config) {
+        RED.nodes.createNode(this, config);
+        var azure = require('azure-storage');
+        var azure_storage_config = RED.nodes.getNode(config.storage);
+        var node = this;
+
+        node.on('input', function (msg) {
+            try {
+                var token = null;
+
+                var container = config.container;
+                if(msg.container !== undefined) {
+                    container = msg.container;
+                }
+
+                var prefix = null;
+                if(config.prefix !== undefined) {
+                    prefix = config.prefix;
+                }
+                if(msg.prefix !== undefined) {
+                    prefix = msg.prefix;
+                }
+
+                node.status({fill: "blue", shape: "dot", text: "Connecting to Azure..."});
+                var all_entries = [];
+                var page = 0;
+
+                var service = azure_storage_config.getService(msg);
+                var options = {};
+                var _lock = true;
+                var _processpage = function(error, entries, response) {
+                    node.status({fill: "blue", shape: "dot", text: "Processing page " + page});
+                    if (error) {
+                        node.status({fill: "red", shape: "dot", text: error});
+                        return node.error(error, msg);
+                    }
+
+                    for(var i = 0; i < entries.entries.length; i++) {
+                        all_entries.push(entries.entries[i]);
+                    }
+
+                    if(!entries.continuationToken) {
+                        msg.payload = all_entries;
+                        node.status({});
+                        return node.send(msg);
+                    }
+                    token = continuationToken;
+                    fetch_page();
+                };
+                var fetch_page = function() {
+                    page += 1;
+                    node.status({fill: "blue", shape: "dot", text: "Fetching page " + page});
+                    if(prefix) {
+                      service.listBlobDirectoriesSegmentedWithPrefix(container, prefix, token, options, _processpage);
+                    } else {
+                      service.listBlobDirectoriesSegmented(container, token, options, _processpage);
+                    }
+                };
+                fetch_page();
+            } catch (err) {
+              node.status({fill: "red", shape: "dot", text: err.message});
+              node.error(err.message);
+            }
+        });
+    }
+    RED.nodes.registerType("azure-storage-listblobdirectories", AzureStorageListBlobDirectoriesNode);
+};
